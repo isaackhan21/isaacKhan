d3aad6d392d8d6bf6151d6c206de985bee4f38b8
Isaac Khan <isaackhan21@gmail.com>
2022-12-05 09:44:24 +0000

    Initial Commit

---
+++ task/libs/js/script.js
@@ -0,0 +1,35 @@
+
+
+$('#button2').click(function() {
+
+    $.ajax({
+        url: "libs/php/API2.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            lat: $('#selLat').val(),
+            lng: $('#selLng').val()
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == "ok") {
+
+                $('#txtName').html(result['data'][0]);
+                $('#txtGeonameId').html(result['data'][1]);
+                $('#txtDistance').html(result['data'][2]);
+                console.log('success');
+               
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // your error code
+            console.log('failed');
+            console.log (errorThrown)
+        }
+    }); 
+
+});
+
