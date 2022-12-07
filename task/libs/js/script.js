e7edba15385b5f9733fec5221e8986400c6106d4
Isaac Khan <isaackhan21@gmail.com>
2022-12-07 09:05:49 +0000

    fixed Ocean API error, earthquakes API

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -1,4 +1,36 @@
+$('#button1').click(function() {
 
+    $.ajax({
+        url: "libs/php/API1.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            north: $('#setNorth').val(),
+            south: $('#setSouth').val(),
+            east: $('#setEast').val(),
+            west: $('#setWest').val()
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if (response.status.name == "ok") {
+                // $('#txtDistance').text(response['data']["distance"]);
+                // $('#txtGeonameId').text(response['data']["geonameId"]);
+                // $('#txtName').text(response['data']["name"]);
+                
+               
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // your error code
+            console.log('failed');
+            console.log (errorThrown)
+        }
+    }); 
+
+});
 
 $('#button2').click(function() {
 
@@ -12,12 +44,12 @@ $('#button2').click(function() {
         },
         success: function(response) {
 
-            console.log(JSON.stringify(result));
+            console.log(JSON.stringify(response));
 
-            if (result.status.name == "ok") {
-                $('#txtName').text(response['data']["name"]);
-                $('#txtGeonameId').text(response['data']["geonameId"]);
+            if (response.status.name == "ok") {
                 $('#txtDistance').text(response['data']["distance"]);
+                $('#txtGeonameId').text(response['data']["geonameId"]);
+                $('#txtName').text(response['data']["name"]);
                 
                
             }
--- task/libs/php/API1.php
+++ task/libs/php/API1.php
@@ -7,7 +7,7 @@
 
 	$executionStartTime = microtime(true);
 
-	$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=flightltd&style=full';
+	$url='http://api.geonames.org/earthquakesJSON?formatted=true&north=' . $_REQUEST['north'] . '&south=' . $_REQUEST['south'] . '&east=' . $_REQUEST['east'] . '&west=' . $_REQUEST['west'] . '&username=awesomecoder28&style=full';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -24,7 +24,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
+	$output['data'] = $decode['earthquakes'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
