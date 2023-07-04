e184304987e4d1f48e0b8276af595bd8bc953285
Isaac Khan <isaackhan21@gmail.com>
2023-07-04 12:51:19 +0100

    Changes to Contact Form

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,9 +1,28 @@
+
+
 $(document).ready(function() {
   $('.error-message, .sent-message').hide();
   $('form.contact-form').submit(function(event) {
     event.preventDefault();
 
-    var formData = $(this).serialize();
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var message = $('#message').val();
+
+    var mailBody = "<h3>New Email from the Contact Form</h3>";
+    mailBody += "<p><strong>Name:</strong> " + name + "</p>";
+    mailBody += "<p><strong>Email:</strong> <a href='mailto:" + email + "'>" + email + "</a></p>";
+    mailBody += "<p><strong>Subject:</strong> " + subject + "</p>";
+    mailBody += "<p><strong>Message:</strong></p>";
+    mailBody += "<p>" + message + "</p>";
+
+    var formData = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: mailBody
+    };
     
 
     $.ajax({
@@ -14,6 +33,7 @@ $(document).ready(function() {
         $('.loading').fadeIn();
       },
       success: function(response) {
+        
         $('.loading').fadeOut();
         $('.sent-message').text(response).fadeIn().delay(3000).fadeOut();
         $('form.contact-form')[0].reset();
--- libs/php/send_email.php
+++ libs/php/send_email.php
@@ -3,24 +3,25 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 require 'phpmailer/src/Exception.php';
 require_once 'config.php';
 
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-
-
-
 $name = $_POST['name'];
-$email = $_POST['email']; 
+$email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
+
+$to = 'isaackhan21@gmail.com'; 
+
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/html\r\n";
 
 $mail = new PHPMailer();
 
@@ -33,12 +34,12 @@ $mail->SMTPSecure = $emailEncryption;
 $mail->Port = $emailPort;
 
 $mail->setFrom($email, $name);
-$mail->addAddress('isaackhan21@gmail.com', 'Isaac');
+$mail->addAddress($to); 
 $mail->Subject = $subject;
 $mail->Body = $message;
+$mail->IsHTML(true);  
 
 $mail->addReplyTo($email, $name);
-
 
 if ($mail->send()) {
   echo 'Email sent successfully.';
